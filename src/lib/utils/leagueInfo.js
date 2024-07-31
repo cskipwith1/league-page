f56c843c1e4a40ea116b28a78c59dd55c7037e0f
Chris Skipwith <99406339+cskipwith1@users.noreply.github.com>
2024-07-31 13:57:04 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "917422806714028032"; // your league ID
+export const leagueID = "1048274888391360512"; // your league ID
 export const leagueName = "Extraordinarily Fresh"; // your league name
 export const dues = 105; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
