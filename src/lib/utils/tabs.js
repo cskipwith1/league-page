afc7538ed08507ba7fae96078321558d5b3ffbcf
Chris Skipwith <99406339+cskipwith1@users.noreply.github.com>
2025-09-16 12:49:47 -0400

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -1,35 +1,30 @@
 import {leagueID} from '$lib/utils/leagueInfo';
 
 export const tabs = [
-    {
-        icon: 'home',
-        label: 'Home',
-        dest: '/',
-        key: 'home',
+        {
+        icon: 'sports_football',
+        label: 'League Website',
+        dest: 'https://extraordinarilyfreshffl.com/',
     },
     {
         icon: 'sports',
         label: 'Matchups',
         dest: '/matchups',
-        key: 'matchups',
     },
     {
         icon: 'swap_horiz',
         label: 'Trades & Waivers',
         dest: '/transactions',
-        key: 'transactions',
     },
     {
         icon: 'article',
         label: 'Blog',
         dest: '/blog',
-        key: 'blog',
     },
     {
         icon: 'view_comfy',
         label: 'League Info',
         nest: true,
-        key: 'league_info',
         children: [
             {
                 icon: 'storage',
@@ -38,8 +33,8 @@ export const tabs = [
             },
             {
                 icon: 'groups',
-                label: 'Managers',
-                dest: '/managers',
+                label: 'Keeper Values',
+                dest: 'https://docs.google.com/spreadsheets/d/1Ya-mA1_mfW0fhw86iZPtERizyGuUVzS0pzGCl8kbyDs/edit?usp=sharing',
             },
             {
                 icon: 'local_fire_department',
@@ -66,22 +61,11 @@ export const tabs = [
                 label: 'Records',
                 dest: '/records',
             },
-            {
-                icon: 'history_edu',
-                label: 'Constitution',
-                dest: '/constitution',
-            },
-            {
-                icon: 'sports_football',
-                label: 'Go to Sleeper',
-                dest: `https://sleeper.app/leagues/${leagueID}`,
-            },
         ]
     },
     {
         icon: 'lightbulb',
-        label: 'Resources',
-        dest: '/resources',
-        key: 'resources',
+        label: 'Go to Sleeper',
+        dest: `https://sleeper.app/leagues/${leagueID}`,
     },
 ];
